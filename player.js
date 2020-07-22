9baf0e708040d3a1fe4e0d6b8a643c2c6da0fc43
alexpear <alex@havenlife.com>
2020-07-21 22:04:42 -0400

    Expand Player

--- player.js
+++ player.js
@@ -1,14 +1,24 @@
 // Represents a player and their current state (how much money they have, etc).
 
+const CardGenerator = require('./cardGenerator.js');
+const Client = require('./client.js')
+
 class Player {
-    constructor (name) {
+    constructor (name, client, generatorInfo) {
         this.name = name;
+        this.client = client;
         this.money = 0;
         this.secondsLeft = 600;
 
+        this.cardGenerator = new CardGenerator(faction, generatorInfo);
     }
 
-    
+    example () {
+        return new Player(
+            'Audrey Hepburn',
+            new Client('horror', 'scifi')
+        );
+    }
 
 
 }
